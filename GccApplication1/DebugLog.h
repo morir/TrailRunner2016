a887c3bc30cf6e389959ff6930291c15aab0bce0
tonnura1975 <aimano.fumito@gmail.com>
2016-09-26 18:37:24 +0900

    【トレイルランナー】電源ON後、想定外の動作をする対応
    ・入力電圧エラー、入力パラメータ―エラーログが発生する問題を修正

--- GccApplication1/DebugLog.h
+++ GccApplication1/DebugLog.h
@@ -13,35 +13,35 @@
 #define LOG_FATAL_ON	//<! 致命的なエラーログの出力設定	：常時ON
 #define LOG_ERROR_ON	//<! エラーログの出力設定			：常時ON
 #define LOG_WARN_ON		//<! 警告の出力設定					：常時ON
-#define LOG_INFO_ON		//<! 情報ログの出力設定				：常時ON
+//#define LOG_INFO_ON		//<! 情報ログの出力設定				：常時ON
 //#define LOG_DEBUG_ON	//<! デバッグログの出力設定			：常時OFF
 
 #if defined(LOG_FATAL_ON)
-#define LOG_FATAL(...)	printf("[FATAL] "); printf(__VA_ARGS__);	//!< 致命的なエラーログ出力
+#define LOG_FATAL(...)	{printf("[FATAL] "); printf(__VA_ARGS__);}	//!< 致命的なエラーログ出力
 #else
 #define LOG_FATAL(...)	//!< 致命的なエラーログ出力
 #endif
 
 #if defined(LOG_ERROR_ON)
-#define LOG_ERROR(...)	printf("[ERROR] "); printf(__VA_ARGS__);	//!< エラーログ出力
+#define LOG_ERROR(...)	{printf("[ERROR] "); printf(__VA_ARGS__);}	//!< エラーログ出力
 #else
 #define LOG_ERROR(...)	//!< エラーログ出力
 #endif
 
 #if defined(LOG_WARN_ON)
-#define LOG_WARN(...)	printf("[WARN ] "); printf(__VA_ARGS__);	//!< 警告ログ出力
+#define LOG_WARN(...)	{printf("[WARN ] "); printf(__VA_ARGS__);}	//!< 警告ログ出力
 #else
 #define LOG_WARN(...)	//!< 警告ログ出力
 #endif
 
 #if defined(LOG_INFO_ON)
-#define LOG_INFO(...)	printf("[INFO ] "); printf(__VA_ARGS__);	//!< 情報ログ出力
+#define LOG_INFO(...)	{printf("[INFO ] "); printf(__VA_ARGS__);}	//!< 情報ログ出力
 #else
 #define LOG_INFO(...)	//!< 情報ログ出力
 #endif
 
 #if defined(LOG_DEBUG_ON)
-#define LOG_DEBUG(...)  printf("[DEBUG] "); printf(__VA_ARGS__);	//!< デバッグログ出力
+#define LOG_DEBUG(...)  {printf("[DEBUG] "); printf(__VA_ARGS__);}	//!< デバッグログ出力
 #else
 #define LOG_DEBUG(...)	//!< デバッグログ出力
 #endif
